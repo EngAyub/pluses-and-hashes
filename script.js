a14c0599e02731c65eea88982f8ae22e3d2b984f
ENG AYUUB <moztaphem9@gmal.com>
2023-06-17 20:07:11 +0300

    pluse and hashes done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+
+function plusHashCount(str) {
+    let hashCount = 0;
+    let plusCount = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === '#') {
+        hashCount++;
+      } else if (str[i] === '+') {
+        plusCount++;
+      }
+    }
+    
+    return [hashCount, plusCount];
+  }
+  console.log(plusHashCount("+++++####"));
